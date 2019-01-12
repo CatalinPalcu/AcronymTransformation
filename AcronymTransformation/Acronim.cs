e74472256a0b1a479156344ee8f64a3695de19a0
CatalinPalcu <palcu_catalin@yahoo.com>
2019-01-12 21:59:39 +0200

    Keep It Simple Student

---
+++ AcronymTransformation/Acronim.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcronymTransformation
+{
+    public static class Acronim
+    {
+        public static string TransformInAcronym(string s)
+        {
+            string acr = "";
+            s = s.Trim();
+            while (s.IndexOf("  ") >= 0)
+                s = s.Replace("  ", " ");
+
+            string[] words = s.Split(' ');
+            foreach (string word in words)
+                acr += word[0];
+
+            return acr;
+        }
+    }
+}
